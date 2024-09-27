cda33badc685c1f94a39e0a2471b4ac0fe594884
Jo-9m-n1 <leejongmin1092@gmail.com>
2024-09-27 09:02:53 -0400

    Update a1.py

--- a1.py
+++ a1.py
@@ -10,13 +10,13 @@ FI = input('Enter your grade of the Final: ')
 MF = input('Enter your grade of the Mid-term & Final Preparation: ')
 
 if int(L) >= int(6):
-    L_G = int(100)
+	L_G = int(100)
 else:
-    L_G = int((int(L) / 6) * 100)
+	L_G = int((int(L) / 6) * 100)
     
 if int(Q) >= int(6):
-    Q_G = int(100)
+	Q_G = int(100)
 else:
 	Q_G = int((int(Q) / 6) * 100)
 
-print('Your grade is: ', int((int(L_G)) * 0.20) + (int(Q_G) * 0.15) + ((((float(A1) + float(A2) + float(A3) + float(A4)) / 4) * 0.16) + (((float(M1) + float(M2)) / 2) * 0.25) + ((float(FI)) * 0.18) + ((float(MF)) * 0.06)))
+print('Your grade is: ', round(float((int(L_G)) * 0.20) + (int(Q_G) * 0.15) + ((((float(A1) + float(A2) + float(A3) + float(A4)) / 4) * 0.16) + (((float(M1) + float(M2)) / 2) * 0.25) + ((float(FI)) * 0.18) + ((float(MF)) * 0.06))))
