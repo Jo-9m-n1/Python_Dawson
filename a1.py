483253a44b432ada9de0229a32fb0488a95de30b
Jo-9m-n1 <leejongmin1092@gmail.com>
2024-09-27 08:43:12 -0400

    Update a1.py

--- a1.py
+++ a1.py
@@ -1,15 +1,22 @@
-Lab = input ('Enter your number of the completed Lab: ')
-Quiz = input ('Enter your number of the quizzes completed: ')
-As_1 = input ('Enter your first grade of the assignment: ')
-As_2 = input ('Enter your second grade of the assignment: ')
-As_3 = input ('Enter your third grade of the assignment: ')
-As_4 = input ('Enter your fourth grade of the assignment: ')
-MT_1 = input ('Enter your first grade of the Mid-term: ')
-MT_2 = input ('Enter your second grade of the Mid-term: ')
+L = input ('Enter your number of the completed Lab: ')
+Q = input ('Enter your number of the quizzes completed: ')
+A1 = input ('Enter your first grade of the assignment: ')
+A2 = input ('Enter your second grade of the assignment: ')
+A3 = input ('Enter your third grade of the assignment: ')
+A4 = input ('Enter your fourth grade of the assignment: ')
+M1 = input ('Enter your first grade of the Mid-term: ')
+M2 = input ('Enter your second grade of the Mid-term: ')
 FI = input ('Enter your grade of the Final: ')
 MF = input ('Enter your grade of the Mid-term & Final Preparation: ')
-print ('Your grade is: ', (int((((int(Lab)) / 6) *100) * 0.20) + (((int(Quiz) / 6) * 100) * 0.15) + ((((int(As_1) + int(As_2) + int(As_3) + int(As_4)) / 4) * 0.16) + (((int(MT_1) + int(MT_2)) / 2) * 0.25) + ((int(FI)) * 0.18) + ((int(MF)) * 0.06))))
 
+if int(L) >= int(6):
+    L_G = int(100)
+else:
+    L_G = int((int(L) / 6) * 100)
+    
+if int(Q) >= int(6):
+    Q_G = int(100)
+else:
+	Q_G = int((int(Q) / 6) * 100)
 
-
-
+print ('Your grade is: ', int((int(L_G)) * 0.20) + (int(Q_G) * 0.15) + ((((int(A1) + int(A2) + int(A3) + int(A4)) / 4) * 0.16) + (((int(M1) + int(M2)) / 2) * 0.25) + ((int(FI)) * 0.18) + ((int(MF)) * 0.06)))
