bc396c1e9848b7c7355e35079d80ed259bc89300
Jo-9m-n1 <leejongmin1092@gmail.com>
2025-03-05 22:28:44 -0500

    Update March 5th 2025

--- Sliding_puzzle.py
+++ Sliding_puzzle.py
@@ -1,8 +1,13 @@
+# Jongmin Lee, 2432787
 import random
+
 def tileLabs(n):
     tile_board = []
     for i in range(1, n ** 2):
-        tile_board.append(str(i) + ' ')
+        if i < 10:
+            tile_board.append(str(i) + ' ')
+        else:
+            tile_board.append(str(i))
     tile_board.append('  ')
     return tile_board
         
@@ -13,15 +18,32 @@ def getNewPuzzle(n):
     return puzzle
     
 def findEmptyTile(puzzle):
+    row = 0
+    col = 0
     for i in range(len(puzzle)):
         for j in range(len(puzzle[0])):
             if puzzle[i][j] == '  ':
-                return (i, j)
+                row = i
+                col = j
+    return (row, col)
 
 def nextMove(n):
+    x = 7 * (n ** 2) - 32
     puzzle = getNewPuzzle(n)
+    print(f'You have {x} moves left.')
     while True:
+        if checkWin(puzzle):
+            displayBoard(puzzle)
+            print('-' * 57)
+            print("Congratulations! You have solved the puzzle.")
+            break
+        else:
+            if x == 0:
+                print('Best of luck next time!')
+                break
+
         (i, j) = findEmptyTile(puzzle)
+
         valid_moves = {}
         if i > 0:
             valid_moves['W'] = (i - 1, j)
@@ -33,21 +55,39 @@ def nextMove(n):
             valid_moves['D'] = (i, j + 1)
             
         displayBoard(puzzle)
-        print("Valid moves: ", valid_moves)
+        move_display = ['(W)' if 'W' in valid_moves else '( )',
+                        '(A)' if 'A' in valid_moves else '( )',
+                        '(S)' if 'S' in valid_moves else '( )',
+                        '(D)' if 'D' in valid_moves else '( )']
+        
+        print('     ' + move_display[0])
+        print(move_display[1] + '  ' + move_display[2] + '  ' + move_display[3])
+
         move = input("Enter WASD (or QUIT): ").upper()
         
         if move in valid_moves:
             new_i, new_j = valid_moves[move]
             puzzle[i][j], puzzle[new_i][new_j] = puzzle[new_i][new_j], puzzle[i][j]
-            displayBoard(puzzle)
-            print("Valid moves: ", valid_moves)
         elif move == 'quit'.upper():
             print("Game terminated by user.")
             break
         else:
             print("Invalid move. Try again.")
-            displayBoard(puzzle)
-            print("Valid moves: ", valid_moves)
+        print('-' * 57)
+        x -= 1
+        print(f'You have {x} moves left.')
+
+
+def checkWin(puzzle):
+    n = len(puzzle)
+    correct = 1
+    for i in range(n):
+        for j in range(n):
+            if puzzle[i][j] != '  ':
+                if int(puzzle[i][j]) != correct:
+                    return False
+                correct += 1
+    return True
 
 def displayBoard(board_lst):
     n = len(board_lst)
@@ -70,19 +110,26 @@ def displayBoard(board_lst):
     draw_board += horizontal_div
     print(draw_board.format(*labels))
 
-print(nextMove(3))
 
+print("Welcome to the Sliding Puzzle Game!")
+n = int(input('Enter the size of the dimension of the puzzle board (minimum 3 & maximum 9): '))
+while n < 3:
+    print('Invalid input. Minimum size is 3.')
+    n = int(input('Enter the size of the dimension of the puzzle board (minimum 3 & maximum 9): '))
+while n > 9:
+    print('Invalid input. Maximum size is 9.')
+    n = int(input('Enter the size of the dimension of the puzzle board (minimum 3 & maximum 9): '))
+print('+' + '-' * 54 + '+')
+print(f'| You are playing a {n}x{n} puzzle board.                  |')
+print('| The empty tile is represented by a blank space.      |')
+print('| The goal is to arrange the tiles in ascending order  |')
+print('| from left to right.                                  |')
+print('| You can move the empty tile to the left, right, up,  |')
+print('| or down.                                             |')
+print("+" + "-" * 54 + "+")
+play = input('Press enter if you understand the game').lower()
 
-
-
-
-
-
-
-        
-        
-        
-
-
-
-
+if play == '':
+    nextMove(n)
+else:
+    print('Game terminated by user.')
--- f.py
+++
@@ -1,5 +0,0 @@
-d = {3: 5, 4: 5, 6: 1}
-d_inverted = {}
-for key in d:
-    d_inverted.append(d[key])
-print(d_inverted)
